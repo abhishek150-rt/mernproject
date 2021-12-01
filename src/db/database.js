48fd4ad7e26e69e07969a4ed31141bc629a93238
Abhishek <abhisheknegi969@gmail.com>
2021-12-01 18:52:17 +0530

    my mern project

--- src/db/database.js
+++ src/db/database.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/studentRegistration", {
+mongoose.connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log("Connection Successful"))
